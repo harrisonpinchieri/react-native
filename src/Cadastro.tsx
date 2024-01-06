e677d5f00a4df69a20878b0d14c2ef87e36ba5af
harrison <harrisonpinchieri@gmail.com>
2024-01-06 04:41:14 -0300

    adicionando checkbox

--- src/Cadastro.tsx
+++ src/Cadastro.tsx
@@ -1,5 +1,5 @@
 
-import { VStack, Image, Text, Box, FormControl, Input, Button, Link, } from 'native-base'
+import { VStack, Image, Text, Box, FormControl, Input, Button, Link, Checkbox } from 'native-base'
 import { TouchableOpacity } from 'react-native';
 import Logo from './assets/Logo.png'
 import { Titulo } from './componentes/Titulo';
@@ -8,9 +8,10 @@ import { Botao } from './componentes/Botao';
 import { useState } from 'react';
 
 export default function Login() {
-
+  //utilizado para ter valores 
   const [numSecao, setNumSecao] = useState(0);
 
+  //cria secoes de navegação
   const secoes = [
     {
       id: 1,
@@ -24,10 +25,10 @@ export default function Login() {
         {
           id: 2,
           label: 'Email',
-          placeholder: 'Digite seu email Completo'
-        }
-      ]
-
+          placeholder: 'Digite seu email'
+        },
+      ],
+      checkbox: []
     },
     {
       id: 2,
@@ -37,9 +38,28 @@ export default function Login() {
           id: 1,
           label: 'CEP',
           placeholder: 'Digite seu CEP'
+        },
+      ],
+      checkbox: []
+    },
+    {
+      id: 3,
+      titulo: 'Para finalizar, quais são os seus planos?',
+      entradaTexto: [],
+      checkbox: [
+        {
+          id: 1,
+          value: 'SulAmerica'
+        },
+        {
+          id: 2,
+          value: 'Unimed'
         }
       ]
     }
+
+
+
   ]
 
   function avancarSecao() {
@@ -69,13 +89,27 @@ export default function Login() {
       <Box>
 
         {
-          secoes[numSecao].entradaTexto.map(entrada => {
+          secoes[numSecao].checkbox.map(checkbox => {
+            return <Checkbox key={checkbox.id} value={checkbox.value}>
+              {checkbox.value}
+            </Checkbox>
+          })
+        }
+
+
+      </Box>
+
+      <Box>
+
+        {
+          secoes[numSecao]?.entradaTexto?.map(entrada => {
             return <EntradaTexto label={entrada.label} placeholder={entrada.placeholder} key={entrada.id} />
           })
         }
 
 
       </Box>
+
 
 
       {numSecao > 0 && <Botao onPress={() => voltarSecao()} bgColor={'gray.400'}>Voltar</Botao>}
